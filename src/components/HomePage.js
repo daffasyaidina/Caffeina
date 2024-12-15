a5fed5327c6a0b14d98f4afa572e44cf107ebba0
daffasyaidina <91464359+daffasyaidina@users.noreply.github.com>
2024-12-16 01:05:30 +0700

    changed the font

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import "./HomePage.css";
 
-// HomePage component
 const HomePage = () => {
   const [products, setProducts] = useState([]);
 
-  // Fetch products from the server
   useEffect(() => {
     fetch("http://localhost:5000/api/products") // Fetch products from the server
       .then((response) => response.json())
@@ -14,7 +12,6 @@ const HomePage = () => {
       .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
-  // Render the component
   return (
     <div className="home-container">
       <header className="header">
@@ -22,10 +19,12 @@ const HomePage = () => {
           <h1 className="logo">About Us</h1>
           <div className="about-section">
             <p className="about-description">
-              Caffeina is a website specializing in Indonesian coffee beans, offering a curated
-              selection from regions like Sumatra, Java, and Sulawesi. With detailed descriptions of
-              each coffee's flavor and origin, Caffeina provides a seamless and inviting shopping
-              experience for coffee enthusiasts, celebrating the rich heritage of Indonesian coffee.
+              Caffeina is a website specializing in Indonesian coffee beans,
+              offering a curated selection from regions like Sumatra, Java, and
+              Sulawesi. With detailed descriptions of each coffee's flavor and
+              origin, Caffeina provides a seamless and inviting shopping
+              experience for coffee enthusiasts, celebrating the rich heritage
+              of Indonesian coffee.
             </p>
           </div>
         </div>
@@ -36,13 +35,15 @@ const HomePage = () => {
           {products.length > 0 ? (
             products.map((product) => (
               <Link
-                to={`/product/${product._id}`} // route to product details 
+                to={`/product/${product._id}`}
                 key={product._id}
                 className="product-link"
               >
                 <div className="product-card">
                   <h3 className="product-title">{product.name}</h3>
-                  <p className="product-price">IDR {product.price.toLocaleString()} for 250g</p>
+                  <p className="product-price">
+                    IDR {product.price.toLocaleString()} for 250g
+                  </p>
                 </div>
               </Link>
             ))
